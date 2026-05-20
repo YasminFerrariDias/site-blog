d8644dea30c8639f9c4fdf7a22a74a78fd0b95b9
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-20 16:33:27 -0300

    Componente: Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { Header } from '@/components/header';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 export default function Home() {
   return (
-    <>
-      <h2>Hello World</h2>
-      <Button variant={"secondary"}>dvs</Button>
-    </>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black`}
+    >
+      <Header />
+    </div>
   );
 }
