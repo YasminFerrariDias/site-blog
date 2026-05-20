b17830b6ea61f205484283a3644f69d6155242c1
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-20 14:03:26 -0300

    Instalando o Tailwind CSS na v3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <h2>Hello World</h2>
-    </div>
+ return (
+    <h2>Hello World</h2>
   );
 }
