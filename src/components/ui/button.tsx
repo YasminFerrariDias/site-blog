aa8201054ba7c5802b31f3abe83bdec2a04f183e
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-21 16:38:53 -0300

    Revert "Refatorações"
    This reverts commit 34bb54841f3f3d80df8ba8cc7d989c73664f6145.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,8 +9,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: 'bg-blue-200 text-white hover:bg-blue-100 rounded-full',
+        default: 'bg-blue-200 text-white hover:bg-blue-300',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        primary:
+          'bg-blue-200 text-white hover:bg-blue-100 rounded-full',
+        outline:
+          'border border-gray-400 bg-gray-700 transition-colors duration-200 hover:text-blue-200 hover:border-blue-200',
         secondary: 'bg-gray-100 text-gray-800 hover:bg-blue-100 rounded-full',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
@@ -20,7 +28,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: 'primary',
+      variant: 'default',
       size: 'default',
     },
   }
