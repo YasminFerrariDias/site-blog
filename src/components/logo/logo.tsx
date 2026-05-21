7784bd7184c58ccda87f014b7c9446290d15164c
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-21 08:35:47 -0300

    Criando a Logo

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const Logo = () => {
+  return (
+    <Link href="/" title="Página inicial">
+      <Image src="/logo.svg"
+        alt="Logo do Site"
+        width={116}
+        height={32}
+      />
+    </Link>
+  )
+}
