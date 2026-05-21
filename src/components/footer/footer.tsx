b8a93c47f56252d697331a915bc4647209df13ba
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-21 08:29:16 -0300

    Componente: Footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const Footer = () => {
+  return (
+    <footer className="border-t border-white/10">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between md:flex-row md:justify-start gap-8 py-8">
+          <Link href="/">
+            <Image src="/logo.svg"
+              alt="Logo do Site"
+              width={116}
+              height={32}
+            />
+          </Link>
+
+          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
+            <Link href="/termos-de-uso" className="hover:text-primary">Termos de uso</Link>
+            <Link href="/politica-de-privacidade">Política de privacidade</Link>
+            <Link href="/feedback">Feedback</Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
+}
