b917b4b0464cb9c98d5a8c63979d32215c119337
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-21 09:01:55 -0300

    Adicionando Style Guide

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,16 @@
 import { Footer } from "../footer"
 import { Header } from "../header"
+import { Inter } from 'next/font/google'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative flex min-h-screen flex-col dark bg-black">
+    <div className={`${inter.className}relative flex min-h-screen flex-col dark bg-gray-700`}>
       <Header />
       <main className="flex-1 flex flex-col mb-12">
         {children}
