940ecaabc1715f6b00a02bd5ce3513ab110d5dbd
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-25 10:08:39 -0300

    Componente: Avatar

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -4,13 +4,21 @@ import Image from "next/image"
 
 import { allPosts } from "contentlayer/generated"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import { Avatar } from "@/components/avatar"
 
 export default function PostPage() {
   const router = useRouter()
   const slug = router.query.slug as string
+
+  if (!slug) {
+    return <div>Carregando...</div>
+  }
+
   const post = allPosts.find((post) =>
     post.slug.toLowerCase() == slug.toLocaleLowerCase()
   )
+
+  const publishedDate = new Date(post?.date).toLocaleDateString('pt-BR')
 
   return (
     <main className="mt-32 text-gray-100">
@@ -33,13 +41,31 @@ export default function PostPage() {
       <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
         <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
           <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-            <Image 
+            <Image
               src={post?.image ?? ''}
               alt={post?.title ?? ''}
               fill
               className="object-cover"
             />
           </figure>
+
+          <header className="p-4 md:p-6 lg:p-12 pb-0">
+            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+              {post?.title}
+            </h1>
+
+            <Avatar.Container>
+              <Avatar.Image src={post?.author.avatar} alt={post.title} />
+              <Avatar.Content>
+                <Avatar.Title>{post?.author.name}</Avatar.Title>
+                <Avatar.Description>
+                  Publicado em {''}
+                  <time dateTime={post?.date ?? ''}>{publishedDate}</time>
+                </Avatar.Description>
+              </Avatar.Content>
+            </Avatar.Container>
+          </header>
+
         </article>
       </div>
     </main>
