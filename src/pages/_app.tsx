1c241656cd8d43c192eb67fdcb4ddd7c75ec2eed
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-25 16:23:36 -0300

    Migrando a Landing Page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import { Layout } from "../components/layout";
-import "@/styles/globals.css";
+
+import { Layout } from "lucide-react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
