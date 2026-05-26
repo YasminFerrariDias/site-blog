56013d5c7aad231db17779be4e51a7a690f5804d
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-26 11:13:34 -0300

    Criando a Página de Post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,22 @@
-import { notFound } from 'next/navigation'
-import { allPosts } from 'contentlayer/generated'
-import { PostPage } from '@/templates/blog/post-page'
+import { allPosts } from "@/.contentlayer/generated";
+import PostPage from "@/pages/blog-page/[slug]";
+import { notFound } from "next/navigation";
 
-type Props = {
-  params: { slug: string }
+type BlogPostPage = {
+  params:Promise<{
+    slug: string
+  }>
 }
 
-export async function generateStaticParams() {
-  return allPosts.map((post) => ({ slug: post.slug }))
-}
+export default async function BlogPostPage({params}: BlogPostPage) {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug == slug)
 
-export default function PostSlugPage({ params }: Props) {
-  const { slug } = params
+  if (!post) {
+    notFound()
+  }
 
-  const post = allPosts.find((p) => p.slug === slug)
-
-  if (!post) return notFound()
-
-  return <PostPage post={post} />
+  return (
+    <PostPage post={post} />
+  )
 }
