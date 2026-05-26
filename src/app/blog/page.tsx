4552cc3393974c8ccff9963d2be6560b28cdf4aa
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-26 16:10:31 -0300

    Melhorando SEO na Página de Listagem de Posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import BlogList from "@/templates/blog/blog-list";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu négocio',
+  robots: 'index follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu négocio',
+    url: '#CAMINHO POSTADO + A FOTO OG',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: '#CAMINHO POSTADO + A FOTO OG',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set - Blog'
+      }
+    ]
+  }
+}
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
