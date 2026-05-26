31d1204d268d4082d42e3345ea139a85031665dc
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-26 16:22:34 -0300

    Melhorando o SEO da Página do Post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,12 +2,34 @@
 import { notFound } from "next/navigation";
 import { PostPage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 
 type BlogPostPageProps = {
   params: {
     slug: string
   }
 }
+
+export async function generateMetadata({ params }: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug == slug);
+
+  if (!post) {
+    return {}
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author?.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image]
+    }
+  }
+}
+
+export const revalidate = 60
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: 'Site.Set',
   description: 'Venda seus produtos como afiliado em um único lugar',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: '#CAMINHO POSTADO + A FOTO OG',
+    siteName: 'Site.Set',
+    locale: 'pt_BR',
+    type: 'website',
+    images: [
+      {
+        url: '#CAMINHO POSTADO + A FOTO OG',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
