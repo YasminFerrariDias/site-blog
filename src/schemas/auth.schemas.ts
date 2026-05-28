b8307ba3df9afa6ddbcc1eceae09ea1fc952ff55
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-05-28 11:30:44 -0300

    adicionando um campo de validação

--- src/schemas/auth.schemas.ts
+++ src/schemas/auth.schemas.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
 
+export const namePeopleSchema = z.object({
+  namePeople: z.string().trim().min(1, "O nome completo é obrigatório."),
+});
+
 export const emailSchema = z.object({
   email: z
     .string()
@@ -7,14 +11,6 @@ export const emailSchema = z.object({
     .min(1, "O email é obrigatório.")
     .email("E-mail inválido")
     .toLowerCase(),
-});
-
-export const passwordSchema = z.object({
-  password: z
-    .string()
-    .trim()
-    .min(1, "A senha é obrigatória.")
-    .min(8, "A senha deve ter no mínimo 8 caracteres"),
 });
 
 export const telephoneSchema = z.object({
@@ -28,10 +24,19 @@ export const telephoneSchema = z.object({
     ),
 });
 
-export const namePeopleSchema = z.object({
-  namePeople: z.string().trim().min(1, "O nome é obrigatório."),
+export const nameStoreSchema = z.object({
+  nameStore: z.string().trim().min(1, "O nome da loja é obrigatório."),
 });
 
-export const nameStoreSchema = z.object({
-  nameStore: z.string().trim().min(1, "O nome é obrigatório."),
+export const passwordSchema = z.object({
+  password: z.string().trim().min(8, "A senha deve ter no mínimo 8 caracteres"),
+});
+
+export const confirmPasswordSchema = (password: string) =>
+z.object({
+  confirmPassword: z
+    .string()
+    .trim()
+    .min(1, "A confirmação de senha é obrigatório.")
+    .refine((val) => val === password, "As senhas não conferem")
 });
